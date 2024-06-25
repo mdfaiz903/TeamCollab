68b57768da32a9b9caccf6bc15314ddd3c873e15
mdfaiz903 <mdfaiz.nub@gmail.com>
2024-06-25 21:31:18 +0600

    model creation done and successfully migrate

--- Core/TeamCollab/models.py
+++ Core/TeamCollab/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+
+    
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    owner = models.ForeignKey(User,on_delete=models.CASCADE,related_name='projects')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ProjectMember(models.Model):
+    project = models.ForeignKey(Project,on_delete=models.CASCADE,related_name='members')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    role = models.CharField(max_length=50)
+
+STATUS_CHOICE = [('to_do', 'To Do'), ('in_progress', 'In Progress'), ('done', 'Done')]
+PRIORITY_CHOICE = [('low', 'Low'), ('medium', 'Medium'), ('high', 'High')]
+class Task(models.Model):
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE)
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICE)
+    assigned_to = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE,related_name='tasks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task,on_delete=models.CASCADE,related_name='comments')
+    created_at = models.DateTimeField(auto_now_add = True)
+
+
