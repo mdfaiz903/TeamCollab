7cdf3cb986f4da4b778242d8d67e0149879d241e
mdfaiz903 <mdfaiz.nub@gmail.com>
2024-06-26 00:08:50 +0600

    Project Done

--- Core/TeamCollab/urls.py
+++ Core/TeamCollab/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path,include
 from rest_framework import routers
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 router = routers.DefaultRouter()
 router.register('users',views.UserViewSet)
 router.register('projects',views.ProjectViewSet)
@@ -11,6 +17,8 @@ router.register('comment',views.CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('users/register/', views.RegisterUserView.as_view(), name='register-user'),
-    path('users/login/', views.LoginUserView.as_view(), name='login-user'),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('projects/<int:project_id>/tasks/', views.ProjectTaskListCreateView.as_view(), name='project-tasks'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
--- Core/TeamCollab/views.py
+++ Core/TeamCollab/views.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
 from .models import User, Project, ProjectMember, Task, Comment
 from .serializers import UserSerializer, ProjectSerializer, ProjectMemberSerializer, TaskSerializer, CommentSerializer
 
@@ -11,42 +10,41 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_permissions(self):
+        if self.action in ['create']:
+            self.permission_classes = [AllowAny,]
+        else:
+            self.permission_classes = [IsAuthenticated,]
+        return super().get_permissions()
+
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-class LoginUserView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request, *args, **kwargs):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        user = authenticate(username=username, password=password)
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key})
-        else:
-            return Response({"error": "Invalid Credentials"}, status=status.HTTP_400_BAD_REQUEST)
-
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
 
 class ProjectMemberViewSet(viewsets.ModelViewSet):
     queryset = ProjectMember.objects.all()
     serializer_class = ProjectMemberSerializer
+    permission_classes = [IsAuthenticated]
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
 
 class ProjectTaskListCreateView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         project_id = self.kwargs['project_id']
